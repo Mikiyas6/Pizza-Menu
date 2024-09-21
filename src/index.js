2c63543a3058687f969e09c8c161e784e9cc4648
Mikiyas6 <Mikitedi9@gmail.com>
2024-09-21 20:31:42 +0300

    I learned about the concept of rendering lists and I rendered each pizzas using the map method.

--- src/index.js
+++ src/index.js
@@ -68,31 +68,24 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>;
-      <Pizza
-        name="Pizza Spinaci"
-        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-        photoName="pizzas/spinaci.jpg"
-        price={10}
-      />
-      <Pizza
-        name="Pizza Funghi"
-        ingredients="Tomato, mushroooms"
-        photoName="pizzas/funghi.jpg"
-        price={12}
-      />
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
     </main>
   );
 }
 function Pizza(props) {
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.photoName} />
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.photoName} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price + 2}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price + 2}</span>
       </div>
-    </div>
+    </li>
   );
 }
 function Footer() {
